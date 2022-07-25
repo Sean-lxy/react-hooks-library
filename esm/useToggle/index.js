607d6472448a34220620cfdf571c2436c12dbb46
Sean-lxy <1430109768@qq.com>
2022-07-25 10:20:43 +0800

    feat: update gitignore

--- esm/useToggle/index.js
+++
@@ -1,6 +0,0 @@
-import { useReducer } from 'react';
-const toggleReducer = (state, nextValue) => typeof nextValue === 'boolean' ? nextValue : !state;
-const useToggle = (initialValue) => {
-    return useReducer(toggleReducer, initialValue);
-};
-export default useToggle;
